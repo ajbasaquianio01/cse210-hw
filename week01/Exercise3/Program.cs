9e9e1518b68608b369695c11d01abdaa60c625dd
ajbasaquianio01 <aaronjohnquianio@gmail.com>
2025-01-23 16:45:01 +0800

    w01exercise.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher than that");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower than that");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations, you guessed it!");
+            }
+
+        }                    
     }
 }
