63c9b076aa93bc1410ad975f4945a2cfdd8c3bd6
ajbasaquianio01 <aaaronjohnquianio@gmail.com>
2025-02-15 03:11:21 +0800

    w05_Inherit.cs

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Ian Capanay", "Addition");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jhaylars Alviar", "Fractions", "2.7", "6-32");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Johndel Macalalad", "Philippine History", "The Causes of Philippine Revolution");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
